c24e853e6a53bbe6279db3c354dc1adc8b3b5942
jlemonn1 <javierlimon6@gmail.com>
2024-10-26 21:54:05 +0200

    Navbar y el about us

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import About from './pages/About';
+
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
+            <div className="app-container">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
         </Router>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import { FaMapMarkedAlt, FaUser } from 'react-icons/fa'; // Icons from Font Awesome
+import { TbFountainFilled } from "react-icons/tb";
+import '../style/Navbar.css'; // Importamos el archivo CSS
+
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
+  return (
+    <div className="navbar">
+      <a href="/" className="nav-item">
+        <FaMapMarkedAlt className="nav-icon" />
+        <span className="nav-text">Map</span>
+      </a>
+
+      <a href="/login" className="nav-item">
+        <FaUser className="nav-icon" />
+        <span className="nav-text">Login</span>
+      </a>
+
+      <a href="/about" className="nav-item">
+        <TbFountainFilled className="nav-icon" />
+        <span className="nav-text">About</span>
+      </a>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
